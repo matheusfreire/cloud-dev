a51a5b4ab4922aa8a96e58a5028e5d22e559604e
Matheus Freire <matheus.sfreire@gmail.com>
2025-02-04 23:37:30 -0300

    Awssdk into middleware

--- src/middleware/uploadImageToS3Middleware.js
+++ src/middleware/uploadImageToS3Middleware.js
@@ -3,13 +3,13 @@ import { S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
 import multerS3 from "multer-s3";
 
-const s3 = new S3Client({
+const s3 = AWSXRay.captureAWSv3Client(new S3Client({
     region: process.env['AWS_REGION'],
     credentials: {
         accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
         secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY']
     }
-});
+}));
 
 const s3Storage = multerS3({
     s3: s3,
--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,8 +1,5 @@
-import { capturePrisma } from '@cosva-labs/aws-xray-sdk-prisma';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
-const client = capturePrisma(prisma, {namespace: 'remote'});
 
 export default prisma;
